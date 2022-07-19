3e66117d1825c38b96ddae752a1b8daf49b2d7df
Antonio J. Turel <antonioturel@gmail.com>
2022-07-19 19:41:54 +0200

    [#6][#17][#19] avance

--- src/Controller/pruebaController.php
+++ src/Controller/pruebaController.php
@@ -9,11 +9,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class pruebaController extends AbstractController
 {
-    
+    /**
+     * @Route("/", name="bienvenida")
+     */
     #[Route('/', name: 'bienvenida')]
     public function bienvenida()
     {
         return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/rankings", name="vistaRankings")
+     */
+    #[Route('/rankings', name: 'vistaRankings')]
+    public function vistaRankings()
+    {
+        return $this->render('rankings.html.twig');
     }
 }
 ?>
