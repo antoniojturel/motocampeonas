0ebea6cfa25de80c56d213489adfc802b57b9f3e
Antonio J. Turel <antonioturel@gmail.com>
2022-07-20 09:42:42 +0200

    [#17][#11][#10][#9][#8][#6] Avance

--- src/Controller/pruebaController.php
+++ src/Controller/pruebaController.php
@@ -10,21 +10,57 @@ use Symfony\Component\Routing\Annotation\Route;
 class pruebaController extends AbstractController
 {
     /**
-     * @Route("/", name="bienvenida")
+     * @Route("/", name="vistaBienvenida")
      */
-    #[Route('/', name: 'bienvenida')]
+    #[Route('/', name: 'vistaBienvenida')]
     public function bienvenida()
     {
-        return $this->render('home.html.twig');
+        return $this->render('bienvenida.html.twig');
     }
 
     /**
      * @Route("/rankings", name="vistaRankings")
      */
-    #[Route('/rankings', name: 'vistaRankings')]
-    public function vistaRankings()
+    //#[Route('/rankings', name: 'vistaRankings')]
+    //public function vistaRankings()
+    //{
+    //    return $this->render('rankings.html.twig');
+    //}
+
+    /**
+     * @Route("/ranking_velocidad", name="vistaRankingVelocidad")
+     */
+    #[Route('/ranking_velocidad', name: 'vistaRankingVelocidad')]
+    public function vistaRankingVelocidad()
     {
-        return $this->render('rankings.html.twig');
+        return $this->render('rankingVelocidad.html.twig');
+    }
+
+    /**
+     * @Route("/ranking_motocross", name="vistaRankingMotocross")
+     */
+    #[Route('/ranking_motocross', name: 'vistaRankingMotocross')]
+    public function vistaRankingMotocross()
+    {
+        return $this->render('rankingMotocross.html.twig');
+    }
+
+    /**
+     * @Route("/registros", name="vistaRegistros")
+     */
+    #[Route('/registros', name: 'vistaRegistros')]
+    public function vistaRegistros()
+    {
+        return $this->render('registros.html.twig');
+    }
+
+    /**
+     * @Route("/datos", name="vistaDatos")
+     */
+    #[Route('/datos', name: 'vistaDatos')]
+    public function vistaDatos()
+    {
+        return $this->render('datos.html.twig');
     }
 }
 ?>
