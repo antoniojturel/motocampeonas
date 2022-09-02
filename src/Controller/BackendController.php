7784c50ea6ef722f3e688a423c413cad35124d8a
Antonio J. Turel <antonioturel@gmail.com>
2022-09-02 13:29:26 +0200

    [#13] Avance

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -13,4 +13,26 @@ class BackendController extends AbstractController
     {
         return $this->render('backend/prediccion.html.twig');
     }
+
+ //   #[Route('/login', name: 'app_login')]
+ //   public function index(AuthenticationUtils $authenticationUtils): Response
+ //   {
+//
+ //       // get the login error if there is one
+ //      $error = $authenticationUtils->getLastAuthenticationError();
+ //      // last username entered by the user
+ //      $lastUsername = $authenticationUtils->getLastUsername(); 
+//
+ //      return $this->render('login/index.html.twig', [
+ //           'controller_name' => 'LoginController',
+ //           'last_username' => $lastUsername,
+ //           'error'         => $error,
+ //       ]);
+ //   }
+//
+ //   #[Route('/logout', name: 'app_logout')]
+ //   public function logout(): Response
+ //   {
+
+//    }
 }
